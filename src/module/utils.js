2416966d1ff30b116c886e429c082c3cbee7bb4c
Ederas229 <120315229+Ederas229@users.noreply.github.com>
2024-08-13 14:19:15 +0200

    Update utils.js

--- src/module/utils.js
+++ src/module/utils.js
@@ -1,27 +1,5 @@
 import { MODULENAME } from './const.js';
 
 export function log(message) {
-  if (typeof message == 'string') {
-    console.log(MODULENAME + ' | ' + message);
-  } else {
-    console.log(MODULENAME + ' |');
-    console.log(message);
-  }
-}
-
-export async function addDaily(filePath, key) {
-  //get the code from the file
-  const code = await (await fetch(filePath)).text();
-  //get all the customs dailies
-  const customs = game.settings.get('pf2e-dailies', 'customDailies');
-  if (!customs) return;
-  const dailyApparitionIndex = customs.findIndex((e) => e.key == key);
-  //if there is already a custom daily for the apparition edit it
-  if (dailyApparitionIndex !== -1) {
-    customs.splice(dailyApparitionIndex, 1, { key: key, code: code, schema: '3.0.0' });
-  } else {
-    //else create it
-    customs.push({ key: key, code: code, schema: '3.0.0' });
-  }
-  game.settings.set('pf2e-dailies', 'customDailies', customs);
+  console.log(MODULENAME + ' | ', message);
 }
